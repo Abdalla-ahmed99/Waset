cbc214cbf05fdcbf15722a78247a490c810e1ad3
Abdalla-ahmed99 <eng.abdalla.ahmed99@gmail.com>
2026-02-19 15:32:56 +0200

    update

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Sidebar.css';
+import './sidebar.css';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteUserDashboard } from '../Redux/UserDashboard';
